4845fde201d331be83b9d9e2f8acf379039d2939
Abhishek Joshi <avjoshi2@illinois.edu>
2024-09-09 13:29:22 -0500

    removed defaults to avoid errors

--- bin/clearFinishedFilesGRMHD.py
+++ bin/clearFinishedFilesGRMHD.py
@@ -4,9 +4,9 @@ import click
 import re
 
 @click.command()
-@click.option("--imagesperdump",default=6,help="number of GRRT parameters (images) per GRMHD dump")
-@click.option("--outputdir",default="./",help="output directory where ipole images are stored")
-@click.option("--grmhddir",default="./",help="directory where all grmhd snapshots are stored")
+@click.option("--imagesperdump",help="number of GRRT parameters (images) per GRMHD dump")
+@click.option("--outputdir",help="output directory where ipole images are stored")
+@click.option("--grmhddir",help="directory where all grmhd snapshots are stored")
 def clearFinishedGRMHD(imagesperdump,outputdir,grmhddir):
     grmhdFiles = sorted(glob.glob(os.path.join(grmhddir,"**/*.h5"),recursive=True))
     imageFiles = sorted(glob.glob(os.path.join(outputdir,"**/*.h5"),recursive=True))
